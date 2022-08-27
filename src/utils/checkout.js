f6e11e775546f00960ded285e5a37599f77ab721
digitalkoncept <digitalkoncept@outlook.com>
2022-08-27 18:26:08 +0530

    update checkout

--- src/utils/checkout.js
+++ src/utils/checkout.js
@@ -119,9 +119,9 @@ const getStripeLineItems = (products) => {
         return {
             quantity: product?.qty ?? 0,
             name: product?.name ?? '',
-            // images: [product?.image?.sourceUrl ?? ''],
-            price: Math.round(product?.price * 100),
-            // currency: 'usd',
+            images: [product?.image?.sourceUrl ?? ''],
+            amount: Math.round(product?.price * 100),
+            currency: 'usd',
         }
     })
 }
